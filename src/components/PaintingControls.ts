fe0f8a57c701999aae6bf67d7320cf5920a85579
Emmanuel Ogbizi <iamogbz@gmail.com>
2026-08-13 06:12:37 +0000

    Fix missing imports in PaintingControls

--- src/components/PaintingControls.ts
+++ src/components/PaintingControls.ts
@@ -1,3 +1,25 @@
+import { html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { SignalElement } from "../utils/SignalElement";
+import {
+  PALETTE_COLOR,
+  PaletteColor,
+  UsedColorStat,
+} from "../types";
+import {
+  selectedCategorySignal,
+  activeHighlightColorSignal,
+  copiedHexSignal,
+  artworksSignal,
+  currentArtworkSignal,
+  isProcessingSignal,
+  isGalleryOpenSignal,
+  zoomScaleSignal,
+  draggedColorSignal,
+  draggedPositionSignal,
+  undoStackSignal,
+  handleUndo,
+} from "../state/store";
 import {
   iconPalette,
   iconPaintBucket,
