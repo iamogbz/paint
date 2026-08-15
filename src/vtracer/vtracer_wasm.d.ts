6e2d98efb1e68bf6f1939aa0317a153048bfb58c
iamogbz <iamogbz@gmail.com>
2026-08-15 01:27:26 +0000

    feat: add vtracer WASM and JS wrapper files

---
+++ src/vtracer/vtracer_wasm.d.ts
@@ -0,0 +1,3 @@
+export default function init(url: string): Promise<void>;
+export function vectorize_bytes(data: Uint8Array, options: any): string;
+export function vectorize_rgba(data: Uint8Array | Uint8ClampedArray, width: number, height: number, options: any): string;
