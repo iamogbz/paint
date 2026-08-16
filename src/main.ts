20a0e561f1b727d9b9112ce6400788bb918be3ec
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2026-08-16 14:03:27 -0400

    fix: add back auto-update polling and with registerSW

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
+import { registerSW } from "virtual:pwa-register";
 import "./index.css";
 import "./App";
+
+registerSW({
+  onRegisteredSW(swUrl, r) {
+    if (r) {
+      setInterval(async () => {
+        if (r.installing || !navigator) return;
+
+        if ("connection" in navigator && !navigator.onLine) return;
+
+        const resp = await fetch(swUrl, {
+          cache: "no-store",
+          headers: {
+            cache: "no-store",
+            "cache-control": "no-cache",
+          },
+        });
+
+        if (resp?.status === 200) await r.update();
+      }, 5 * 60 * 1000); // 5 minutes
+    }
+  },
+});
