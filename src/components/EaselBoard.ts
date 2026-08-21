iamogbz/paint

commit a6b6f0915fc79ac49c70e62503ecd83362ac3144

fix: ensure artwork state is always in sync no matter starting state

src/components/EaselBoard.ts
@@ -625,9 +625,10 @@ export class EaselBoard extends SignalElement {
     if (!expected || expected === TRANSPARENT_HEX) return;
 
     const regionHasBrushStrokes = Object.keys(currentArtwork.brushStrokePaths[regionId] ?? {}).length > 0;
-    // no color change, no brushes to replace
-    if (currentColor === colorHex && !regionHasBrushStrokes) return;
-    pushUndoState(currentArtwork);
+    // theres a color change or brushes to replace
+    if (currentColor !== colorHex || !regionHasBrushStrokes) {
+      pushUndoState(currentArtwork);
+    }
 
     // Unconditionally remove all brush strokes clipped by this region when a fill is applied
     if (currentArtwork.brushStrokePaths) delete currentArtwork.brushStrokePaths[regionId];

commit b93104199de728004ee71f57b9b474cfd12744c6

fix: bug where undo was added even when no state changed
introduced by a6b6f09

src/components/EaselBoard.ts
@@ -630,7 +630,7 @@ export class EaselBoard extends SignalElement {
 
     const regionHasBrushStrokes = Object.keys(currentArtwork.brushStrokePaths[regionId] ?? {}).length > 0;
     // theres a color change or brushes to replace
-    if (currentColor !== colorHex || !regionHasBrushStrokes) {
+    if (currentColor !== colorHex || regionHasBrushStrokes) {
       pushUndoState(currentArtwork);
     }
 