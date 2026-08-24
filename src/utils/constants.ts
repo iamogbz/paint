7259a58531c65c61ee944f6c45525e956831ddfc
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2026-08-23 20:42:03 -0400

    fix: merge similar colors after regions haev been decided

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,12 +8,7 @@ export const FALLBACK_IMAGE_SIZE_PX = 960;
 export const MIN_ZOOM = 0.5;
 export const MAX_ZOOM = 16.0;
 
-export const UNSHARP_MASK_THRESHOLD = 10;
-export const UNSHARP_MASK_AMOUNT = 0.6;
-export const BILATERAL_SPATIAL_SIGMA = 1.5;
-export const BILATERAL_RANGE_SIGMA = 25.0;
-export const SHARPEN_AMOUNT = 0.5;
-export const COLOR_COLLAPSE_DELTA_E_THRESHOLD = 1.0;
+export const COLOR_COLLAPSE_DELTA_E_THRESHOLD = 3.0;
 
 const fillableSvgElements = ["circle", "ellipse", "path", "polyline", "polygon", "rect", "text", "textPath", "tspan"] as const;
 export const FILLABLE_SVG_ELEMENTS = new Set(fillableSvgElements);
